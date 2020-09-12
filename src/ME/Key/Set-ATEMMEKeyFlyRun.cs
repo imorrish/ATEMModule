87c10f9bc2a2abd15c423767797a080dd25e90b5
Ian Morrish <ian_morrish@hotmail.com>
2020-09-13 07:28:56 +1200

    Starting on USK set commands

---
+++ src/ME/Key/Set-ATEMMEKeyFlyRun.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.Generic;
+using LibAtem.Commands;
+using LibAtem.Commands.MixEffects;
+using LibAtem.Commands.MixEffects.Key;
+using LibAtem.Commands.MixEffects.Transition;
+using LibAtem.Common;
+using LibAtem.Net;
+
+namespace ATEMModule
+{
+    [Cmdlet(VerbsCommon.Set,"ATEMMEKeyFlyRunTo")]
+        [OutputType(typeof(bool))]
+public class ATEMMEKeyFlyRunTo : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public AtemClient ATEMref { get; set; }
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public int MEID { get; set; }
+        [Parameter(
+            Mandatory = true,
+            Position = 2,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public int KeyerIndex { get; set; }
+        [ValidateSet("None","A","B","Full","RunToInfinite")]
+        [Parameter(
+            Mandatory = true,
+            Position = 3,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public int KeyFrame { get; set; }
+        [ValidateSet("CentreOfKey","TopLeft","TopCentre","TopRight","MiddleLeft","MiddleCentre","MiddleRight","BottomLeft","BottomCentre","BottomRight")]
+        [Parameter(
+            Mandatory = true,
+            Position = 4,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public int RunToInfinite { get; set; }
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+        protected override void ProcessRecord()
+        {
+            ATEMref.SendCommand(new MixEffectKeyFlyRunSetCommand {MixEffectIndex = (MixEffectBlockId)MEID, KeyerIndex = (UpstreamKeyId)KeyerIndex,KeyFrame=(FlyKeyKeyFrameType)KeyFrame,RunToInfinite=(FlyKeyLocation)RunToInfinite});
+            WriteObject(true);
+        }
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+}
