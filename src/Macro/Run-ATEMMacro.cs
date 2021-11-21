3adc5af9f4920c00e306de557e7c6bba63e19256
Ian Morrish <ian_morrish@hotmail.com>
2021-11-21 13:46:46 +1300

    Updated framework and streaming latency cmd

---
+++ src/Macro/Run-ATEMMacro.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.Generic;
+using LibAtem.Commands;
+using LibAtem.Commands.Macro;
+using LibAtem.Common;
+using LibAtem.Net;
+
+namespace ATEMModule
+{
+[Cmdlet(VerbsCommon.Set ,"ATEMMacro")]
+        [OutputType(typeof(bool))]
+        
+    public class ATEMRunMacro : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public AtemClient ATEMref { get; set; }
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true)]
+        public uint MacroID { get; set; } =0;
+        protected override void BeginProcessing()
+        {
+            WriteVerbose("Begin!");
+        }
+
+        // This method will be called for each input received from the pipeline to this cmdlet; if no input is received, this method is not called
+        protected override void ProcessRecord()
+        {
+            ATEMref.SendCommand(new MacroActionCommand {Index = MacroID, Action=(MacroActionCommand.MacroAction)0});
+            WriteObject(true);
+        }
+
+        // This method will be called once at the end of pipeline execution; if no input is received, this method is not called
+        protected override void EndProcessing()
+        {
+            WriteVerbose("End!");
+        }
+    }
+}
